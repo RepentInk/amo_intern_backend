3dd8c76db433e948ba4c55e26a3d6cd9a1d2ec57
Swanzy Benjamin <benjaminswanzy@outlook.com>
2023-07-20 15:45:30 +0000

    uerlog function added

---
+++ src/services/userLog.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+
+interface UserLog {
+  id: number;
+  user_id: number;
+  action: string;
+  model: string;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date;
+}
+
+@Injectable()
+export class UserLogService {
+  private userLogs: UserLog[] = [];
+
+  logUserActivity(user_id: number, action: string, model: string): string {
+    const userLog: UserLog = {
+      id: this.userLogs.length + 1,
+      user_id,
+      action,
+      model,
+      created_at: new Date(),
+      updated_at: new Date(),
+      deleted_at: null,
+    };
+
+    this.userLogs.push(userLog);
+
+    const message = `${user_id} ${action} a ${model}`;
+
+    return message;
+  }
+}
