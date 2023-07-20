edc069ce29af4d2100638a9b170ad3928f70a34d
DaniellaMomo <momodaniella218@gmail.com>
2023-07-20 22:04:55 +0000

    added comments

--- src/controllers/rolepermissions.controller.ts
+++ src/controllers/rolepermissions.controller.ts
@@ -25,7 +25,6 @@ export class RolePermissionsController {
 
   @Get()
   getAllRolePermissions(): RolePermissions[] {
-    // return this.rolePermissionsService.getAllRolePermissions();
     return this.rolePermissionsService.getAllRolePermissions();
   }
 
--- src/services/rolepermissions.service.ts
+++ src/services/rolepermissions.service.ts
@@ -4,28 +4,31 @@ import { RolePermissions } from 'src/dto/rolepermissions.dto';
 @Injectable()
 export class RolePermissionsService {
   private allRolePermissions = [];
+  //adding  new rolepermission
   createRolePermission(newRolePermission: RolePermissions) {
     this.allRolePermissions.push(newRolePermission);
     return newRolePermission;
   }
+  //getting all the rolepermissions
   getAllRolePermissions(): RolePermissions[] {
     return this.allRolePermissions;
   }
-
+  //getting rolepermission based on id
   getRolePermissionsById(id: number) {
     const findRolePermission = this.allRolePermissions.find(
       (rolePermission) => rolePermission.id == id,
     );
     return findRolePermission;
   }
+  //updating an existing rolepermission
   updateRolePermission(id: number, updateRolePermission: RolePermissions) {
     this.allRolePermissions = this.allRolePermissions.map((rolePermission) => {
       if (rolePermission.id == id) {
         return { ...rolePermission, ...updateRolePermission };
       } else return rolePermission;
     });
-    return this.getRolePermissionsById(id);
   }
+  //delete a rolepermission
   deleteRolePermission(id: number) {
     this.allRolePermissions = this.allRolePermissions.filter(
       (rolePermission) => rolePermission.id !== id,
