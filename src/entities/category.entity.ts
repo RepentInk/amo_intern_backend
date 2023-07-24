15719601b9f32fe0f3deaf04e4c331d7c19256a4
Swanzy Benjamin <benjaminswanzy@outlook.com>
2023-07-24 15:53:12 +0000

    items, customer & categories added

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Items } from './items.entiy';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Items, (item) => item.category)
+  items: Items[];
+
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({ default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updated_at: Date;
+
+  @Column({ nullable: true })
+  deleted_at: Date;
+}
