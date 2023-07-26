9b9e305265b9af6ccbff1429210d795829068552
zark28 <abdulrazakadams28@gmail.com>
2023-07-26 01:51:21 +0000

    update database configuration

--- src/database/dbconfig.ts
+++ src/database/dbconfig.ts
@@ -1,4 +1,8 @@
-export const dbConfig = {
+import { ConfigModule } from '@nestjs/config/dist';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+ConfigModule.forRoot();
+export const dbConfig: MysqlConnectionOptions = {
+  type: 'mysql',
   host: process.env.HOST_NAME,
   port: parseInt(process.env.PORT, 10),
   username: process.env.DB_USERNAME,
