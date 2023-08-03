310db18c011aa132d99bdf57977548dc2d70a712
zark28 <abdulrazakadams28@gmail.com>
2023-08-03 10:38:55 +0000

    update readme file

--- src/database/datasource.config.ts
+++ src/database/datasource.config.ts
@@ -14,5 +14,5 @@ export default new DataSource({
   database: process.env.DB_NAME,
   entities: entities,
   synchronize: false,
-  migrations: [Dbmigration1691053913018],
+  migrations: [Dbmigration1691053913018]
 });
