3835f200af494e7a63af5da08f3f898d4ca91f54
zark28 <abdulrazakadams28@gmail.com>
2023-08-07 16:33:05 +0000

    create swagger documentation for permission resource

--- src/controllers/role.controller.ts
+++ src/controllers/role.controller.ts
@@ -13,28 +13,43 @@ import {
 import { RoleService } from '../services/role.service';
 import { BasicController } from 'src/interfaces/controller.interface';
 import { RoleDto } from 'src/dto/role.dto';
-
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+@ApiBearerAuth()
+@ApiTags('role')
 @Controller('roles')
 export class RoleController implements BasicController {
   constructor(private readonly roleService: RoleService) {}
 
   @Get()
+  @ApiOperation({ summary: 'get all roles' })
+  @ApiResponse({ status: 200, description: 'successful' })
   findAll(): Promise<RoleDto[]> {
     return this.roleService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'get one role' })
+  @ApiResponse({ status: 200, description: 'role found' })
   findOne(@Param('id', ParseIntPipe) id: number): Promise<RoleDto> {
     return this.roleService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'create role' })
+  @ApiResponse({ status: 200, description: 'role created' })
   @UsePipes(new ValidationPipe())
   create(@Body() roleDto: RoleDto): Promise<RoleDto> {
     return this.roleService.create(roleDto);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'update role' })
+  @ApiResponse({ status: 200, description: 'role updated' })
   update(
     @Body() roleDto: RoleDto,
     @Param('id', ParseIntPipe) id: number,
@@ -43,6 +58,8 @@ export class RoleController implements BasicController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'delete role' })
+  @ApiResponse({ status: 200, description: 'role deleted' })
   delete(@Param('id', ParseIntPipe) id: number) {
     return this.roleService.delete(id);
   }
--- src/dto/role.dto.ts
+++ src/dto/role.dto.ts
@@ -1,8 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { MinLength } from 'class-validator';
 export class RoleDto {
+  @ApiProperty({
+    example: 1,
+    description: 'unique identifyer for the row of inpu',
+  })
   id: number;
+
+  @ApiProperty({
+    example: 'admin',
+    description: 'access names given to users',
+  })
   @MinLength(3)
   name: string;
+  @ApiProperty({
+    example: 'admin can create, view, update and delete all entities',
+    description: 'description of functional ability of an access name',
+  })
   description?: string;
   created_at?: Date;
   updated_at?: Date;
