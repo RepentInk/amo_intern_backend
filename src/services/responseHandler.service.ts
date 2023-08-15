2b8d69790e9f5fbc89570d2720cf9164bbdbc767
IsaacNyarko <nyarkorep@gmail.com>
2023-08-15 09:36:10 +0000

    fixing

--- src/services/responseHandler.service.ts
+++ src/services/responseHandler.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ResponseHandlerService {
+  
   successResponse(
     data: any,
     message: string = 'Successful',
@@ -15,14 +16,15 @@ export class ResponseHandlerService {
     };
   }
 
-  errorResponse(message: string, statusCode: HttpStatus) {
+  errorResponse(message: string, error: any, statusCode: HttpStatus) {
     throw new HttpException(
       {
         error: true,
         error_message: message,
-        data: null,
+        data: error,
       },
       statusCode
     );
   }
+
 }
